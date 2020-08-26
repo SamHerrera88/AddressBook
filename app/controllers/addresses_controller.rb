a1b32611f76aa6994d46a68cce72734fa3a005ac
Jung Diána <Dindi@Dius-MacBook-Air.local>
2020-08-26 18:39:27 +0200

    routes are off

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,7 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: [:show, :edit, :update, :destroy]
+  before_action :set_person
+
 
   # GET /addresses
   # GET /addresses.json
@@ -28,7 +30,7 @@ class AddressesController < ApplicationController
 
     respond_to do |format|
       if @address.save
-        format.html { redirect_to @address, notice: 'Address was successfully created.' }
+        format.html { redirect_to @person, notice: "Address was successfully created." }
         format.json { render :show, status: :created, location: @address }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class AddressesController < ApplicationController
   def update
     respond_to do |format|
       if @address.update(address_params)
-        format.html { redirect_to @address, notice: 'Address was successfully updated.' }
+        format.html { redirect_to @person, notice: "Address was successfully updated." }
         format.json { render :show, status: :ok, location: @address }
       else
         format.html { render :edit }
@@ -56,7 +58,7 @@ class AddressesController < ApplicationController
   def destroy
     @address.destroy
     respond_to do |format|
-      format.html { redirect_to addresses_url, notice: 'Address was successfully destroyed.' }
+      format.html { redirect_to @person, notice: "Address was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -69,6 +71,10 @@ class AddressesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def address_params
-      params.require(:address).permit(:street, :town, :zipcode, :state, :country)
+      params.require(:address).permit(:street, :town, :zipcode, :state, :country, :person_id)
+    end
+
+    def set_person
+      @person = Person.find(params[:person_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'people#index'
 
-  resources :emails
-  resources :phones
-  resources :people
-  resources :addresses
+  resources :people do
+    resources :emails
+    resources :phones
+    resources :addresses
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
