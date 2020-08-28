933148c8a1653d99ba7eea53d638105774790d88
Jung Diána <Dindi@Dius-MacBook-Air.local>
2020-08-29 00:01:15 +0200

    test are done

--- spec/views/addresses/_index.html.erb_spec.rb
+++ spec/views/addresses/_index.html.erb_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "addresses/_index", type: :view do
       Address.create!(
         street: "Street",
         town: "Town",
-        zipcode: "Zipcode",
+        zipcode: 1234,
         state: "State",
         country: "Country",
         person: @person,
@@ -15,7 +15,7 @@ RSpec.describe "addresses/_index", type: :view do
       Address.create!(
         street: "Street",
         town: "Town",
-        zipcode: "Zipcode",
+        zipcode: 1234,
         state: "State",
         country: "Country",
         person: @person,
@@ -27,7 +27,7 @@ RSpec.describe "addresses/_index", type: :view do
     render
     assert_select "tr>td", text: "Street".to_s, count: 2
     assert_select "tr>td", text: "Town".to_s, count: 2
-    assert_select "tr>td", text: "Zipcode".to_s, count: 2
+    assert_select "tr>td", text: 1234.to_s, count: 2
     assert_select "tr>td", text: "State".to_s, count: 2
     assert_select "tr>td", text: "Country".to_s, count: 2
   end
