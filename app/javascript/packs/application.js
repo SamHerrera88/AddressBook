9c8f121fa7219b9a2fa419763f1d19dffbf76e5d
Jung Diána <Dindi@Dius-MacBook-Air.local>
2020-08-31 01:22:37 +0200

    updated js packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -21,4 +20,5 @@ import '../stylesheets/application'
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').tooltip()
 })
